30288585a03dee96c622786b6fce59b74a031a08
Chanbadsha <chanbadsha005@gmail.com>
2024-12-03 22:29:00 +0600

    make some change

---
+++ index.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const express = require('express');
+const port = process.env.PORT || 5000
+const app = express()
+
+// Middleware 
+app.use(express.json())
+app.use(cors())
+
+
+// Default Code
+app.get('/', (req, res) => {
+    res.send("Global Gate Server Is Ready")
+})
+app.listen(port, () => {
+    console.log(`Global Gate server is running on port: ${port}`);
+})
